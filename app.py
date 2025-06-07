86bfaf283a1ff84c8d6547cb28c89b63b2961cdc
Robin3880 <robinkonrad101@gmail.com>
2025-06-07 22:36:12 +0100

    added login and register

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, redirect, g, request, session
 from flask_session import Session
 from database import get_db, close_db 
+from functools import wraps 
 from werkzeug.security import generate_password_hash, check_password_hash
+from forms import RegisterForm, LoginForm
 
 
 
@@ -14,6 +16,61 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
+@app.before_request
+def load_logged_in_user():
+    g.user = session.get("username", None)
+
+def login_required(view):
+    @wraps(view)
+    def wrapped_view(*args,**kwargs):
+        if g.user is None:
+            return redirect(url_for("login", next=request.url))
+        return view(*args,**kwargs)
+    return wrapped_view
+
+
 @app.route("/")
+@login_required
 def index():
     return render_template("index.html")
+
+@app.route("/register", methods = ["POST","GET"])
+def register():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        username = form.username.data
+        if username.strip() == "":
+            form.username.errors.append("username must have characters")
+            return render_template("register.html", form = form)
+        password = form.password1.data
+        db = get_db()
+        username_in_db = db.execute('''SELECT * FROM users WHERE username = ?;''', (username,)).fetchone()
+        if username_in_db == None:
+            db.execute('''INSERT INTO users VALUES (?, ?);''',(username, generate_password_hash(password)))
+            db.commit()
+        else:
+            form.username.errors.append("username already taken")
+        return redirect(url_for("login"))
+    return render_template("register.html", form=form)
+
+
+@app.route("/login", methods = ["POST", "GET"])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        username = form.username.data
+        password = form.password.data
+        db = get_db()
+        username_in_db = db.execute('''SELECT * FROM users WHERE username = ?;''', (username,)).fetchone()
+        if username_in_db == None:
+            form.username.errors.append(f"user {username} does not exist")
+        elif not check_password_hash(username_in_db["password"], password):
+            form.password.errors.append("Incorrect Password")
+        else:
+            session["username"] = username
+            session.modified = True
+            next_page = request.args.get("next")
+            if not next_page:                   
+                next_page = url_for("index")
+            return redirect(next_page)
+    return render_template("login.html", form=form)
--- forms.py
+++ forms.py
@@ -1,3 +1,15 @@
 from flask_wtf import FlaskForm 
-from wtforms import SubmitField, PasswordField
-from wtforms.validators import InputRequired
+from wtforms import StringField, SubmitField, PasswordField
+from wtforms.validators import InputRequired, EqualTo
+
+class RegisterForm(FlaskForm):
+    username = StringField("username:", validators=[InputRequired()])
+    password1 = PasswordField("password:", validators=[InputRequired()])
+    password2 = PasswordField("confirm password:", validators=[InputRequired(), EqualTo("password1","passwords must match")])
+    submit = SubmitField("Register")
+
+
+class LoginForm(FlaskForm):
+    username = StringField("username:", validators=[InputRequired()])
+    password = PasswordField("password:", validators=[InputRequired()])
+    submit = SubmitField("Log in")
