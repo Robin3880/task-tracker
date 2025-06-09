15d7725601db62fdba18f948546a2d8a8130f235
Robin3880 <robinkonrad101@gmail.com>
2025-06-09 10:57:35 +0100

    added marking as doing and done functionality

--- static/main.js
+++ static/main.js
@@ -1,6 +1,8 @@
 
 const new_button = document.querySelector("#new");
 const to_do = document.querySelector("#to-do");
+const doing = document.querySelector("#doing");
+const done = document.querySelector("#done");
 new_button.addEventListener("click", createCard);
 
 function createCard() {
@@ -9,14 +11,29 @@ function createCard() {
     card.className = "card";
     const title = document.createElement("h2");
     const description = document.createElement("p");
+    const mark_doing = document.createElement("button");
     card.append(title);
     card.append(description);
+    card.append(mark_doing);
     title.innerHTML = "test";
     title.contentEditable = "true";
     description.innerHTML = "description";
     description.contentEditable = "true";
-    
-    
-
+    mark_doing.innerHTML = "mark as doing";
+    mark_doing.addEventListener("click", markDoing);
 }
 
+function markDoing(event) {
+    const mark_doing = event.target;
+    const card = mark_doing.parentElement;
+    doing.append(card);
+    mark_doing.innerHTML = "mark as done"
+    mark_doing.addEventListener("click", markDone);
+}
+
+function markDone(event) {
+    const mark_done = event.target;
+    const card = mark_done.parentElement;
+    done.append(card);
+    card.removeChild(mark_done);
+}
