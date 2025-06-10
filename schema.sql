0b4b07990d2cac788e7f3d37f568039c9d773048
Robin3880 <robinkonrad101@gmail.com>
2025-06-10 20:24:14 +0100

    added cards sql schema

--- schema.sql
+++ schema.sql
@@ -3,3 +3,25 @@ CREATE TABLE users
     username TEXT PRIMARY KEY,
     password TEXT NOT NULL
 );
+
+-- CREATE TABLE groups
+-- (
+--     username TEXT PRIMARY KEY,
+--     password TEXT NOT NULL
+-- );
+
+-- CREATE TABLE pages (
+--     id INTEGER PRIMARY KEY,
+--     title TEXT NOT NULL,
+--     created_by INTEGER,
+--     FOREIGN KEY (created_by) REFERENCES users(id)
+-- );
+
+CREATE TABLE cards (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    title TEXT,
+    description TEXT,
+    status TEXT DEFAULT "to-do"
+--  created_by INTEGER,
+--  FOREIGN KEY (created_by) REFERENCES users(id)
+);
